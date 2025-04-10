90135f96477d266a27057a89191cfc4389dc0596
Muhammad Za'im Fauwaz Bin Mohd Zain <zaimefc@outlook.com>
2025-04-10 20:35:35 +0800

    Incremental Assessment Commit (v3)
    - Added sleep() function to imitate the time taken for each flow to complete.
    - Added user information table (name, current plan, and registered servers) for the particular user.
    - Set the server connection boundaries for each plan (business rules - https://runcloud.io/pricing).
    - Renamed the main file from main.php to runcloud.php
    
    Note: To show the autoload.php file, the vendor folder (generated by composer) is also commited.

--- Controllers/User.php
+++ Controllers/User.php
@@ -4,6 +4,9 @@ namespace Project;
 use Project\Plans;
 use Project\Server;
 
+// Important Note: sleep() command is used to imitate the delays/flows as performed by the actual operations (such as saving information into database).
+// In actual operations, these delays are vary depending on the time taken by the operation to complete.
+
 class User{
 
     // Attributes are set here (public and private)
@@ -13,9 +16,11 @@ class User{
 
     public function subscribe(Plans $plan)
     {
+        sleep(1);
         // In this scenario, upgrading or downgrading server plans are possible, as long the current customer does not subscribe to the same plan for more than one time.
         print "Action: Subscribing to " . $plan->getPlanType() . "\n";
 
+        sleep(2.5);
         // Occurs if the user have not subscribed to any plan yet, or wanted to change plan.
         if ($this->currentPlan === null || $this->currentPlan->getPlanType() !== $plan->getPlanType()) {
             $this->currentPlan = $plan;
@@ -26,12 +31,15 @@ class User{
         elseif ($this->currentPlan->getPlanType() == $plan->getPlanType()) {
             print "Error => User is already subscribed to " . $plan->getPlanType() . "\n\n";
         }
+        sleep(1);
     }
 
     public function connectServer(Server $server)
     {
+        sleep(1);
         print("Action: Connecting to server ".$server->name."\n");
 
+        sleep(2.5);
         // Occurs if the user has not subscribed to any plans.
         if ($this->currentPlan === null){
             print "Error => User is not subscribed to any RunCloud plans\n\n";
@@ -44,11 +52,17 @@ class User{
 
             $this->serverLists[] = $server;
             print "Success => Successfully established connection to server " . $server->name . "\n\n";
+            $this->showInfo($this->name, $this->currentPlan->getPlanType(), $this->serverLists);
         }
+        sleep(1);
     }
 
     public function unsubscribe()
     {
+        sleep(1);
+        print("Action: Unsubscribing current plan ". $this->currentPlan->getPlanType() ."\n");
+
+        sleep(2.5);
         //Occurs if the user decides to unsubscribe current plan.
         if ($this->currentPlan != null) {
             print "Success => User has successfully unsubscribed from RunCloud " . $this->currentPlan->getPlanType() . ".\n";
@@ -60,5 +74,25 @@ class User{
         else {
             print "Error => User is not subscribed to any RunCloud plans.\n\n";
         }
+        sleep(1);
+    }
+
+    // For table output purposes
+    private function showInfo($name, $plan, $servers){
+        sleep(2.5);
+        print "\n".str_repeat('-', 60);
+        // Name
+        print "\nUser's Name\t\t|   ".$name;
+        print "\n".str_repeat('-', 60);
+        // Plan
+        print "\nCurrent Plan\t\t|   ".$plan;
+        print "\n".str_repeat('-', 60);
+        // Servers
+        print "\nServers Available\t|\n";
+        foreach ($servers as $server) {
+            print "\t\t\t|   " . $server->name . "\t[" . $server->ipAddress . "]\n";
+        }
+        print str_repeat('-', 60)."\n"."\n";
+        sleep(1);
     }
 }
